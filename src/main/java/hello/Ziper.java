646c61959507263a90ddcbdfff9750f02c7d0f04
Alexandr Mikryukov <aleks_mikryukov@mail.ru>
2016-11-18 19:16:42 +0300

    zip without dir itself + check file exists

--- src/main/java/hello/Ziper.java
+++ src/main/java/hello/Ziper.java
@@ -8,17 +8,15 @@ public class Ziper {
 
     public static void main(String[] args) throws Exception {
 
-        File file1 = new File(args[1]);
+        String sourceFolderName =  args[0];
+        String outputFileName = args[1];
 
-        try (ZipOutputStream out = new ZipOutputStream(new FileOutputStream(stripExtension(file1.getCanonicalPath()) + ".zip"))) {
+        try (ZipOutputStream out = new ZipOutputStream(new FileOutputStream(stripExtension(outputFileName)+".zip"))) {
 
-            File file2 = new File(args[0]);
+            doZip(sourceFolderName, out,sourceFolderName);
 
-            doZip(file2, out,"");
-        } catch (FileNotFoundException e) {
-            throw e;
-        } catch (IOException e) {
-            throw e;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
@@ -34,19 +32,29 @@ public class Ziper {
             return str.substring(0, pos);
         }
 
-    private static void doZip(File dir, ZipOutputStream out, String start) throws IOException{
-        String name = start + dir.getName();
-        if (dir.listFiles().length > 0) {
-            for (File f : dir.listFiles()) {
-                if (f.isDirectory()) {
-                    doZip(f, out, name + "/");
-                } else {
-                    out.putNextEntry(new ZipEntry(name + "/" + f.getName()));
-                    write (new FileInputStream(f), out);
+    private static void doZip(String folderName, ZipOutputStream out, String baseFolderName) throws Exception{
+        File f = new File(folderName);
+        if (f.exists()) {
+
+            if (f.isDirectory()) {
+
+                if (!folderName.equalsIgnoreCase(baseFolderName)) {
+                    String entryName = folderName.substring(baseFolderName.length() + 1, folderName.length()) + File.separatorChar;
+                    ZipEntry ze = new ZipEntry(entryName);
+                    out.putNextEntry(ze);
                 }
+                File f2[] = f.listFiles();
+                for (int i = 0; i < f2.length; i++) {
+                    doZip(f2[i].getAbsolutePath(), out, baseFolderName);
+                }
+            } else {
+                String entryName = folderName.substring(baseFolderName.length() + 1, folderName.length());
+                out.putNextEntry(new ZipEntry(entryName));
+                write(new FileInputStream(folderName), out);
             }
-        } else {
-            out.putNextEntry(new ZipEntry(name + "/"));
+        }
+        else {
+        System.out.println("File or directory not found " + folderName);
         }
     }
 
